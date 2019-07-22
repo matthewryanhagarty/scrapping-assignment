fbc0abd1882a4895bdd7059e38c90c0115fd216c
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-21 17:37:27 -0700

    working on server. got scraped articles to display in console, but struggling with everything else.

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var db = require("./models");
+
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
@@ -28,6 +30,70 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/", function(req, res) {
     res.render("index");
+  });
+
+app.get("/scrape", function(req, res) {
+
+    axios.get("https://blog.feedspot.com/music_blogs/").then(function(response) {
+
+        var $ = cheerio.load(response.data);
+      
+      
+        $("a.tlink").each(function(i, element) {
+
+        var result = {};
+      
+          result.title = $(element).text();
+      
+          result.link = $(element).attr("href");
+    
+
+          db.Article.create(result)
+          .then(function(dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+        })
+        // res.redirect("/");
+        
+      });
+});
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+    
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
   });
 
 
