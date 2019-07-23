1c42582aaef6c69f48d73c58c967ef4d463708b1
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-22 18:19:15 -0700

    working with amber on the homework and continuing to make progress. will continue styling tonight.

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
-
 var exphbs = require("express-handlebars");
 
+
+
 var db = require("./models");
-
 var PORT = 3000;
-
 var app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -28,13 +27,6 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
-app.get("/", function(req, res) {
-    res.render("index");
-  });
-
-
-
-
     
 app.get("/scrape", function(req, res) {
 
@@ -42,64 +34,91 @@ app.get("/scrape", function(req, res) {
 
         var $ = cheerio.load(response.data);
         
-        
         $("a.tlink").each(function(i, element) {
 
-        var result = {};
+            var result = {};
         
             result.title = $(element).text();
-        
             result.link = $(element).attr("href");
     
 
             db.Article.create(result)
             .then(function(dbArticle) {
-            console.log(dbArticle);
+            // console.log(dbArticle);
             })
             .catch(function(err) {
             console.log(err);
             });
         }) 
-        res.send("Blogs scraped")
+          res.json("Scrape Complete")
         
         })
 });
 
-
-app.get("/articles/:id", function(req, res) {
-    db.Article.findOne({ _id: req.params.id })
-      .populate("note")
-      .then(function(dbArticle) {
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
-
-app.get("/articles", function(req, res) {
+app.get("/", function(req, res) {
     db.Article.find({})
       .then(function(dbArticle) {
-        res.json(dbArticle);
+
+        res.render("home", {
+          articles: dbArticle
+        })
+      })
+      .catch(function(err) {
+        // res.json(err);
+      });
+  });
+
+
+  app.get("/saved", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.render("saved", {
+          articles: dbArticle
+        })
       })
       .catch(function(err) {
         res.json(err);
       });
   });
 
-  app.post("/articles/:id", function(req, res) {
-    db.Note.create(req.body)
-      .then(function(dbNote) {
+
+// app.get("/articles/:id", function(req, res) {
+//     db.Article.findOne({ _id: req.params.id })
+//       .populate("note")
+//       .then(function(dbArticle) {
+//         res.json(dbArticle);
+//       })
+//       .catch(function(err) {
+//         res.json(err);
+//       });
+//   });
+
+//   app.post("/articles/:id", function(req, res) {
+//     db.Note.create(req.body)
+//       .then(function(dbNote) {
     
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
+//         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//       })
+//       .then(function(dbArticle) {
+//         res.json(dbArticle);
+//       })
+//       .catch(function(err) {
+//         res.json(err);
+//       });
+//   });
+
+
+  app.put("/articles/:id", function(req, res) {
+
+    db.Article.findOneAndUpdate({_id: req.params.id}, req.body)
+    .then(function(response) {
+      res.json(true)
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+
+  })
 
 
 app.listen(PORT, function() {
